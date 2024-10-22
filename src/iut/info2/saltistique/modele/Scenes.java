e45497d0ea5a626b4ad6c3ba4775de70872139c7
Hugo Robles <hugo@robles.com>
2024-10-22 09:49:26 +0200

    Importation de l'énumération de gestion des scènes

---
+++ src/iut/info2/saltistique/modele/Scenes.java
@@ -0,0 +1,41 @@
+/**
+ * Scenes.java                      SAE 3.A.01
+ * IUT de RODEZ, tous les droits sont réservés
+ */
+package iut.info2.saltistique.modele;
+
+/**
+ * L'énumération {@code Scenes} représente les différentes scènes de l'application JavaFX.
+ * <p>Chaque élément de cette énumération correspond à une scène spécifique de l'application,
+ * et est associé au chemin du fichier FXML correspondant qui définit l'interface utilisateur de la scène.</p>
+ *
+ * <p>Cette énumération fournit également une méthode pour récupérer le chemin du fichier FXML associé à chaque scène.</p>
+ *
+ * @author Hugo ROBLES
+ */
+public enum Scenes {
+
+    ACCUEIL("/iut/info2/saltistique/vue/accueil.fxml"),
+    IMPORTATION_RESEAU("/iut/info2/saltistique/vue/importerReseau.fxml");
+
+    /** Le chemin du fichier FXML correspondant à la scène. */
+    private String chemin;
+
+    /**
+     * Constructeur de l'énumération {@code Scenes}.
+     *
+     * @param chemin le chemin du fichier FXML associé à la scène
+     */
+    Scenes(String chemin) {
+        this.chemin = chemin;
+    }
+
+    /**
+     * Retourne le chemin du fichier FXML associé à la scène.
+     *
+     * @return le chemin du fichier FXML sous forme de chaîne de caractères
+     */
+    public String getChemin() {
+        return chemin;
+    }
+}
