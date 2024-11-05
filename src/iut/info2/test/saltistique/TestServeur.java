aab4a9a92fe8808b459306deb0ed40427405e3c9
Hugo Robles <hugo@robles.com>
2024-11-05 11:22:54 +0100

    Create TestServeur.java

---
+++ src/iut/info2/test/saltistique/TestServeur.java
@@ -0,0 +1,26 @@
+package iut.info2.test.saltistique;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestServeur {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void run() {
+    }
+
+    @Test
+    void arreter() {
+    }
+}
