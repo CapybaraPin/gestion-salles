d317eb4b775d1934fe79d35c1af4ee522cb4f132
Hugo Robles <hugo@robles.com>
2024-11-05 14:28:10 +0100

    Importation des tests unitaires

--- src/iut/info2/test/saltistique/TestFichier.java
+++ src/iut/info2/test/saltistique/TestFichier.java
@@ -1,29 +1,152 @@
 package iut.info2.test.saltistique;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import iut.info2.saltistique.modele.Fichier;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Classe de test de gestion des fichiers
+ */
 class TestFichier {
 
+    /** sortieStandard du terminal */
+    private final PrintStream sortieStandard = System.out;
+    private final ByteArrayOutputStream capteurDeSortie = new ByteArrayOutputStream();
+
+    /** Lien vers les fichiers */
+    public static final String[] LIEN_FICHIERS = {
+            "src/test/resources/activites.csv",
+            "src/test/resources/employes.csv",
+            "src/test/resources/reservations.csv",
+            "src/test/resources/salles.csv"
+    };
+
+    /** Nom des fichiers sans leurs extentions */
+    public static final String[] NOM_FICHIER = {
+            "activites",
+            "employes",
+            "reservations",
+            "salles"
+    };
+
+    /** Objets fichiers */
+    public static final Fichier[] OBJET_FICHIER = new Fichier[] {
+            new Fichier("src/test/resources/activites.csv"),
+            new Fichier("src/test/resources/employes.csv"),
+            new Fichier("src/test/resources/reservations.csv"),
+            new Fichier("src/test/resources/salles.csv")
+    };
+
+    /** Contenu des fichiers */
+    public static final String[][] CONTENU_DE_FICHIERS = {
+            {"ABCD"},
+            {"Lorem", "", "Ipsum"},
+            {"a", "", "", "",  "b"},
+            {"", "", "",  ""},
+            {"Lorem", "DELETE FROM mysql.user", "Ipsum",  "Dolor sit amet"},
+            {}
+    };
+
+    /** Résultats des contenus des fichiers */
+    public static final String[] RESULTAT_POUR_CONTENU = {
+            "ABCD\r\n",
+            "Lorem\r\n\r\nIpsum\r\n\r\n",
+            "a\r\n\r\n\r\n\r\nb\r\n",
+            "\r\n\r\n\r\n\r\n",
+            "Lorem\r\nDELETE FROM mysql.user\r\nIpsum\r\nDolor sit amet\r\n",
+            ""
+    };
+
+    /** Validité du statut des fichiers */
+    public static final boolean[] EXTENSIONS_VALIDES = {
+            true,
+            true,
+            true,
+            true,
+            true,
+            true
+    };
 
     @BeforeAll
     static void setUpBeforeClass() throws Exception {
-        System.out.println("====== TEST DE LA CLASSE Fichier ======");
+        System.out.println("------TEST DE LA CLASSE Fichier------");
     }
 
     @AfterAll
     static void tearDownAfterClass() throws Exception {
-        System.out.println("======= TEST DE Fichier TERMINE =======");
+        System.out.println("---FIN DE TEST DE LA CLASSE Fichier---");
     }
 
     @Test
-    void testFichier() {
-        // TODO: implémenter ici
+    void testExtensionValide(){
+        Fichier fichier;
+
+        for (int indiceTest = 0;
+             indiceTest < EXTENSIONS_VALIDES.length; indiceTest++) {
+
+            fichier = OBJET_FICHIER[indiceTest];
+
+            assertEquals(fichier.extensionValide(), EXTENSIONS_VALIDES[indiceTest]);
+        }
     }
 
     @Test
-    void testContenuFichier() {
-        // TODO: implémenter ici
+    void testContenuFichier() throws IOException {
+        Fichier fichier;
+
+        for (int indiceTest = 0;
+             indiceTest < OBJET_FICHIER.length; indiceTest++) {
+
+            fichier = OBJET_FICHIER[indiceTest];
+
+            assertArrayEquals(CONTENU_DE_FICHIERS[indiceTest], fichier.contenuFichier());
+
+        }
     }
+
+    @Test
+    void testAffichageFichier() throws IOException {
+        Fichier fichier;
+
+        for (int indiceTest = 0;
+             indiceTest < OBJET_FICHIER.length;
+             indiceTest++) {
+
+            fichier = OBJET_FICHIER[indiceTest];
+
+            System.setOut(new PrintStream(capteurDeSortie));
+
+            fichier.affichageFichier();
+
+            assertEquals(RESULTAT_POUR_CONTENU[indiceTest],
+                    capteurDeSortie.toString());
+
+            System.setOut(sortieStandard);
+
+            capteurDeSortie.reset();
+        }
+    }
+
+    @Test
+    void testGetFichierExploite() {
+        Fichier fichier;
+
+        for (int indiceTest = 0;
+             indiceTest < LIEN_FICHIERS.length; indiceTest++) {
+
+            fichier = new Fichier(LIEN_FICHIERS[indiceTest]);
+
+            assertEquals(fichier.getFichierExploite(), OBJET_FICHIER[indiceTest].getFichierExploite());
+
+        }
+    }
+
 }
--- src/iut/info2/test/saltistique/TestServeur.java
+++ src/iut/info2/test/saltistique/TestServeur.java
@@ -1,15 +1,51 @@
 package iut.info2.test.saltistique;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import iut.info2.saltistique.modele.Client;
+import iut.info2.saltistique.modele.Fichier;
+import iut.info2.saltistique.modele.Notification;
+import iut.info2.saltistique.modele.Serveur;
+import org.junit.jupiter.api.*;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TestServeur {
 
+    private final int PORT = 50000;
+    private final String HOST = "localhost";
+
+    private final Fichier[] fichiers = {
+            new Fichier("src/ressources/fichiers/activites.csv"),
+            new Fichier("src/ressources/fichiers/employes.csv"),
+            new Fichier("src/ressources/fichiers/reservations.csv"),
+            new Fichier("src/ressources/fichiers/salles.csv")
+    };
+
+    private Serveur serveur;
+    private Client client;
+
+    TestServeur() throws IOException {
+    }
+
+    @BeforeAll
+    static void setUpBeforeClass() throws Exception {
+        System.out.println("====== TEST DE LA CLASSE Serveur ======");
+    }
+
+    @AfterAll
+    static void tearDownAfterClass() throws Exception {
+        System.out.println("======= TEST DE Serveur TERMINE =======");
+    }
+
     @BeforeEach
     void setUp() {
+        this.serveur = new Serveur(PORT, fichiers);
+        try {
+            this.client = new Client(HOST, PORT);
+        } catch (Notification e) {
+            e.printStackTrace();
+        }
     }
 
     @AfterEach
@@ -18,9 +54,12 @@ class TestServeur {
 
     @Test
     void run() {
+        serveur.run();
     }
 
     @Test
     void arreter() {
+        serveur.arreter();
+
     }
 }
