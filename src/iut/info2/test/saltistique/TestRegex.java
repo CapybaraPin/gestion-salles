1bbd4924bf21fee27b914738c981e86a632786a3
Tom Gutierrez <pro.tom.gutierrez@gmail.com>
2024-11-21 11:42:02 +0100

    Ajout du corps TestRegex

---
+++ src/iut/info2/test/saltistique/TestRegex.java
@@ -0,0 +1,43 @@
+package iut.info2.test.saltistique;
+
+import iut.info2.saltistique.modele.Regex;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestRegex {
+
+
+
+
+    @BeforeAll
+    static void setUpBeforeClass() throws Exception {
+        System.out.println("Début des tests");
+    }
+
+    @AfterAll
+    static void tearDownAfterClass() throws Exception {
+        System.out.println("Fin des tests");
+    }
+
+    @Test
+    void testRegexActivite() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testRegexSalle() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testRegexEmploye() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testRegexReservation() {
+        fail("Not yet implemented");
+    }
+}
