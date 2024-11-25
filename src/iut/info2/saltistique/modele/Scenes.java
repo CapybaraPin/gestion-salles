5bb8964c44096bc6c2fa979ed7539acef93724f7
Dorian8521 <dorian.adams@iut-rodez.fr>
2024-11-25 14:03:23 +0100

    Plus d'information reservation
    Ajout d'un bouton plus d'information pour les Locations et les Formations

--- src/iut/info2/saltistique/modele/Scenes.java
+++ src/iut/info2/saltistique/modele/Scenes.java
@@ -22,7 +22,7 @@ public enum Scenes {
     CONSULTER_DONNEES_INCORRECTES("/iut/info2/saltistique/vue/consulterDonneesIncorrectes.fxml"),
     CONSULTER_SALLE("/iut/info2/saltistique/vue/consulterSalle.fxml"),
     CONSULTER_STATISTIQUES("/iut/info2/saltistique/vue/consulterStatistiques.fxml"),
-    SALLES_NON_RESERVEES("/iut/info2/saltistique/vue/sallesNonReservees.fxml");
+    SALLES_NON_RESERVEES("/iut/info2/saltistique/vue/sallesNonReservees.fxml"),
     INFORMATION_RESERVATION("/iut/info2/saltistique/vue/informationReservation.fxml"),;
 
     /** Le chemin du fichier FXML correspondant à la scène. */
