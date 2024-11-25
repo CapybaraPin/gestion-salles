f89b521268ffd5cb24171e3d109a6e7b8edd6f7c
Tom Gutierrez <pro.tom.gutierrez@gmail.com>
2024-11-25 13:53:05 +0100

    Update Regex.java
    Correction de certains espaces : "Salle A" était refusé par exemple

--- src/iut/info2/saltistique/modele/Regex.java
+++ src/iut/info2/saltistique/modele/Regex.java
@@ -16,8 +16,8 @@ public enum Regex {
         @Override
         public String getRegex(String delimiteur) {
             return "^E\\d{6}" + delimiteur + // Identifiant : 'E' suivi de 6 chiffres
-                    "[^" + delimiteur + "\\s]+" + delimiteur + // Nom : tout caractère sauf le délimiteur
-                    "[^" + delimiteur + "\\s]+" + delimiteur + // Prénom : tout caractère sauf le délimiteur
+                    "([^" + delimiteur + "\\s][^" + delimiteur + "]*)" + delimiteur + // Nom : tout caractère sauf le délimiteur
+                    "([^" + delimiteur + "\\s][^" + delimiteur + "]*)" + delimiteur + // Prénom : tout caractère sauf le délimiteur
                     "(\\d{4}|\\s+)?$"; // Téléphone : 4 chiffres ou vide
         }
     },
@@ -30,17 +30,17 @@ public enum Regex {
         @Override
         public String getRegex(String delimiteur) {
             return "^([0-9]{8})" + delimiteur // Identifiant : exactement 8 chiffres
-                    + "([^" + delimiteur + "\\s]+)" + delimiteur // Nom de la salle : tout caractère sauf le délimiteur
+                    + "([^" + delimiteur + "\\s][^" + delimiteur + "]*)" + delimiteur // Nom de la salle : tout caractère sauf le délimiteur
                     + "([0-9]+)"+ delimiteur // Capacité : un ou plusieurs chiffres
                     + "([onON][^" + delimiteur + "]*)" + delimiteur // Vidéoprojecteur : commence par 'o' ou 'n'
-                                                                    // suivi de tout caractère sauf le délimiteur
+                    // suivi de tout caractère sauf le délimiteur
                     + "([onON][^" + delimiteur + "]*)" + delimiteur // ecranXXL : commence par 'o' ou 'n'
-                                                                    // suivi de tout caractère sauf le délimiteur
+                    // suivi de tout caractère sauf le délimiteur
                     + "([0-9]*|\\s*)"  + delimiteur // Nombre d'ordinateurs : un ou plusieurs chiffres ou vide
                     + "([^" + delimiteur + "]*)" + delimiteur // Type : tout caractère sauf le délimiteur
                     + "([^" + delimiteur + "]*)" + delimiteur // Logiciels : tout caractère sauf le délimiteur
                     + "([onON][^" + delimiteur + "]*|)$"; // Imprimante : commence par 'o' ou 'n'
-                                                          // suivi de tout caractère sauf le délimiteur ou vide
+            // suivi de tout caractère sauf le délimiteur ou vide
         }
     },
     ACTIVITES {
@@ -67,7 +67,7 @@ public enum Regex {
             return "^R\\d{6}" + delimiteur // Identifiant : 'R' suivi de 6 chiffres
                     + "\\d{8}" + delimiteur // Salle : 8 chiffres
                     + "E\\d{6}" + delimiteur // Employé : 'E' suivi de 6 chiffres
-                    + "[^" + delimiteur + "\s]+" + delimiteur // Activité : tout sauf le délimiteur
+                    + "([^" + delimiteur + "\\s][^" + delimiteur + "]*)" + delimiteur // Activité : tout sauf le délimiteur
                     + "\\d{2}/\\d{2}/\\d{4}" + delimiteur // Date : JJ/MM/AAAA
                     + "\\d{1,2}h\\d{2}" + delimiteur // Heure de début : HHhMM
                     + "\\d{1,2}h\\d{2}" + delimiteur // Heure de fin : HHhMM
@@ -88,7 +88,7 @@ public enum Regex {
         public String getRegex(String delimiteur) {
             return "^([^;]*;){5}" + // 5 champs avant le champ ordinateur
                     "([0-9]+);" + // nb ordinateurs (au moins un chiffre)
-                    "([^" + delimiteur + "\\s]+);" + // type d'ordinateur (au moins un caractère)
+                    "([^" + delimiteur + "\\s][^" + delimiteur + "]*)" + delimiteur + // type d'ordinateur (au moins un caractère)
                     "([^" + delimiteur + "]*);" + // logiciels (vide ou au moins un caractère)
                     "([onON][^" + delimiteur + "]*)$"; // imprimante (commence par 'o' ou 'n'
             // suivi de tout caractère sauf le délimiteur)
